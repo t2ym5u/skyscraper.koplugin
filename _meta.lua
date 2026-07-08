b1653b9285953c6aec780f3c1cee5cc7cfeb9031
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-08 14:04:40 +0200

    feat: i18n FR/EN translation + bump to 1.1.0

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "skyscraper",
     fullname    = _("Skyscraper"),
     description = _("Place buildings by skyline clues"),
-    version     = "1.0.1",
+    version     = "1.1.0",
 }
