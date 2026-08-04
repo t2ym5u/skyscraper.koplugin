ab667f730cab99efc9e03011ed7308b92d4807c4
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:51:41 +0200

    chore: bump version to 1.1.12

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Skyscraper"),
     description = _("Place buildings by skyline clues"),
-    version     = "1.1.11",
+    version     = "1.1.12",
 }
