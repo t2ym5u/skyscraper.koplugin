169c1f92a8557a6b6ee416df08aa6dfae74a638f
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:08 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Place buildings by skyline clues"] = { fr = "Placez les gratte-ciel selon les indices de vue" },
-    ["Skyscraper"] = { fr = "Gratte-ciel" },
+    ["Place buildings by skyline clues"] = { fr = "Placez les gratte-ciel selon les indices de vue", es = "Coloca los edificios según las pistas de perfil", de = "Platziere Gebäude nach den Skyline-Hinweisen" },
+    ["Skyscraper"] = { fr = "Gratte-ciel", es = "Rascacielos", de = "Wolkenkratzer" },
 }
